12aaa7776222b8962bfa06df22db04ebd31a09b0
Juho Juopperi <juho.juopperi@remod.fi>
2021-03-19 12:04:29 +0200

    Queue sudoku moves from chat and wait for sudoku to be idle for 4 seconds before applying moves

--- sudokuTwitch.js
+++ sudokuTwitch.js
@@ -1,28 +1,79 @@
+let lastActivity = performance.now(); // DOMHighResTimeStamp
+let requestQueue = [];
+
+/**
+ * Detect mouse clicks and key presses. Keep lastActivity timestamp
+ * updated so we know to avoid doing game moves in middle of user interaction.
+ */
+function activityDetected(event) {
+    lastActivity = event.timeStamp;
+}
+document.body.addEventListener('click', activityDetected);
+document.body.addEventListener('keydown', activityDetected);
+
+/**
+ * Handle one game move
+ */
+function handleRequest(request) {
+    if (request.type === "sudoku") {
+        console.log("Sudoku move cell: " + request.cell + " value: " + request.value);
+
+        // Extract row and column (0-based index)
+        let match = request.cell.toLowerCase().match(/^([a-i])([1-9])$/)
+        let column = match[1].charCodeAt(0) - 97 // Convert column from a-i to 0 based index
+        let row = parseInt(match[2]) - 1 // Convert from 1 based to 0 based index
+
+        // Find the correct game-cell and click it.
+        let cells = document.getElementsByClassName("game-cell");
+        let gameCell = cells[9 * row + column];
+        gameCell.click();
+
+        // Find the correct numpad key and click it.
+        if (request.value) {
+            document.getElementsByClassName("numpad-item")[request.value-1].click();
+            gameCell.classList.add("chat-user-cell");
+        } else {
+            document.getElementsByClassName("game-controls-erase")[0].click();
+            gameCell.classList.remove("chat-user-cell")
+        }
+    }
+}
+
+/**
+ * Process requestQueue if nothing has been clicked in about four seconds.
+ * Otherwise postpone processing and try again shortly.
+ */
+function processQueue() {
+    if (requestQueue.length < 1) {
+        console.log("Nothing to do");
+        return;
+    }
+    let requiredIdle = 4000;
+    let now = performance.now();
+    let idle = now - lastActivity;
+    console.log("Processing queue at " + now + " with last activity at " + lastActivity + " idle " + idle);
+    if (idle > requiredIdle) {
+        console.log("Going to process requests");
+        let request;
+        while ((request = requestQueue.shift())) {
+            console.log("Processing request: " + request);
+            handleRequest(request);
+        }
+    } else {
+        let postponeBy = requiredIdle - idle + 100;
+        console.log("Postponing by " + postponeBy);
+        setTimeout(function () {
+            console.log("Processing queue after postpone of " + postponeBy);
+            processQueue();
+        }, postponeBy);
+    }
+}
+
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        if (request.type === "sudoku") {
-            console.log("Sudoku move cell: " + request.cell + " value: " + request.value);
-
-            // Extract row and column (0-based index)
-            let match = request.cell.toLowerCase().match(/^([a-i])([1-9])$/)
-            let column = match[1].charCodeAt(0) - 97 // Convert column from a-i to 0 based index
-            let row = parseInt(match[2]) - 1 // Convert from 1 based to 0 based index
-
-            // Find the correct game-cell and click it.
-            let cells = document.getElementsByClassName("game-cell");
-            let gameCell = cells[9 * row + column];
-            gameCell.click();
-
-            // Find the correct numpad key and click it.
-            if (request.value) {
-                document.getElementsByClassName("numpad-item")[request.value-1].click();
-                gameCell.classList.add("chat-user-cell");
-            } else {
-                document.getElementsByClassName("game-controls-erase")[0].click();
-                gameCell.classList.remove("chat-user-cell")
-            }
-
-        }
+        console.log("Queue request: " + request)
+        requestQueue.push(request);
+        processQueue();
     }
 );
 
@@ -56,3 +107,4 @@ function inject_numbers () {
     });
 }
 setTimeout(inject_numbers, 1000);
+
